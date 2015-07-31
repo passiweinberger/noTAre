ff806b402c664279d8e1c3ae2b03307770142189
NikolaiDavydov <nikolai.davydov@freenet.de>
2015-08-01 01:23:30 +0200

    Update getType.js

--- getType.js
+++ getType.js
@@ -4,9 +4,10 @@ module.exports = {
         if (url.match(/\.(jpeg|jpg|gif|png)$/) != null){
         	type = 'bild';
 		}
-        if ((url.match(/\.(html|php)$/) != null) or (url.indexOf('.') == -1)) {
+        if ((url.match(/\.(html|php)$/) != null) || (url.lastIndexOf('.') < url.length - 5) || (url.split("/")[2] == url) || url.slice(url.lastIndexOf('.')).match(/\.(de|com|to|es|rus|uk|fr|edu|org)$/) != null) {
         	type = 'seite';
 		}
+//		var type = document.getElementById(url).files[0].type;
 		return type;
 	}
 }
